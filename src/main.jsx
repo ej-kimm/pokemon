0c33cef06ea73b8d9d0cc7db3f35963dc4b6d66e
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-11-06 23:56:23 +0900

    🚚 Update directory structure for GlobalStyle.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { GlobalStyle } from './styles/GlobalStyle.jsx'
+import { GlobalStyle } from './styles/base/GlobalStyle'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
