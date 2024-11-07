45764cdfe70502147b36eea88d7ff5d099bdfd60
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-11-08 00:01:01 +0900

    💄 Add error alert component

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -2,15 +2,26 @@ import React, { useEffect, useState } from 'react'
 import Dashboard from './../components/Dashboard'
 import PokemonList from './../components/PokemonList'
 import { StyledDexLayout } from './../styles/layouts/StyledDex'
+import ErrorAlert from '../styles/modules/ErrorAlert'
 
 export default function Dex() {
   const [pokemons, setPokemons] = useState([])
   const [selectedPokemonList, setSelectedPokemonList] = useState([])
+  const [error, setError] = useState({ message: '', show: false })
 
   const handleSelectPokemon = (pokemon) => {
     setSelectedPokemonList((prev) => {
-      if (prev.length >= 6) return prev
-      if (prev.find((p) => p.id === pokemon.id)) return prev
+      if (prev.length >= 6) {
+        setError({ message: '최대 여섯 개까지만 추가 가능합니다', show: true })
+        return prev
+      }
+      if (prev.find((p) => p.id === pokemon.id)) {
+        setError({
+          message: `${pokemon.korean_name}은(는) 이미 추가되었습니다`,
+          show: true,
+        })
+        return prev
+      }
       return [...prev, pokemon]
     })
   }
@@ -29,6 +40,7 @@ export default function Dex() {
 
   return (
     <StyledDexLayout>
+      {error.show && <ErrorAlert message={error.message} setError={setError} />}
       <Dashboard
         selectedPokemonList={selectedPokemonList}
         onRemovePokemon={handleRemovePokemon}
