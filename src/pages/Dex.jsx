0a10b13f01104d57beff3974ecd96bec23056652
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-11-10 20:42:30 +0900

    🔥 Remove unnecessary error useState

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -5,11 +5,8 @@ import { StyledDexLayout } from './../styles/layouts/StyledDex'
 import { PokemonProvider } from '../context/PokemonContext'
 
 export default function Dex() {
-  const [error, setError] = useState({ message: '', show: false })
-
   return (
     <StyledDexLayout>
-      {error.show && <ErrorAlert message={error.message} setError={setError} />}
       <PokemonProvider>
         <Dashboard />
         <PokemonList />
