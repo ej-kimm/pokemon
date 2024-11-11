d04ff8d7f156bb3bfd9756114dd8ba3609d101ff
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-11-11 15:16:51 +0900

    🐛 Fix button color change issue on Home.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
 import { StyledButton } from '../styles/modules/StyledButtons'
 
-const Button = ({ type = 'button', size = 'small', onClick, children }) => {
+const Button = ({
+  type = 'button',
+  size = 'small',
+  action,
+  onClick,
+  children,
+}) => {
   return (
-    <StyledButton type={type} size={size} onClick={onClick}>
+    <StyledButton type={type} size={size} action={action} onClick={onClick}>
       {children}
     </StyledButton>
   )
